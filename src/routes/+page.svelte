db55372943e54ec08d48ea07493554ae44b3849d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-03-24 14:29:06 +0000

    add text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script>
 </script>
-
+<p>quasiscytal.(6,8)fold</p>
 <!-- <img src="/aufbau.svg" alt="aufbau logo" width="76" height="20" /> -->
 
 <style>
 	img {
 		object-fit: contain;
+	}
+
+	p {
+
+		font-weight: 900;
 	}
 </style>
 
