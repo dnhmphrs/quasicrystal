9cabeebf423d1f743bab6fc1a9204faff617a448
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-03-24 14:45:11 +0000

    edit text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
-<p>quasiscytal.Q(ω,π/2).(8-cat) </p>
+<p>quasiscytal.Q(ω,π/2).(6,8-fold) </p>
 <!-- <img src="/aufbau.svg" alt="aufbau logo" width="76" height="20" /> -->
 
 <style>
